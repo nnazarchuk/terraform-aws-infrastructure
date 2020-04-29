069da060eb517990aebc1957ceb1d74cb6bf9e7e
nnazarchuk <nik.nazarchuk@gmail.com>
2020-04-30 02:11:24 +0600

    remove user-data

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "base" {
   source         = "./base"
   instance_count = var.instance_count
-  user_data      = var.user_data
   common_tags    = var.common_tags
 }
--- variables.tf
+++ variables.tf
@@ -4,45 +4,6 @@ variable "instance_count" {
   default = 1
 }
 
-variable "user_data" {
-  default = <<EOF
-#!/bin/bash
-
-sudo mkdir -p /var/www/app
-sudo bash -c 'cat <<EOF > /var/www/app/index.html
-<html>
-    <head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-        <h1>Hello from $(hostname -I)!</h1>
-    </body>
-</html>
-EOF'
-
-sudo yum -y install epel-release
-sudo yum -y install nginx
-sudo setenforce permissive
-sudo chown nginx:nginx /var/www/app/index.html
-sudo chmod -R 755 /var/www/app/index.html
-
-sudo bash -c 'cat <<EOF > /etc/nginx/conf.d/app.conf
-server {
-    listen 9000;
-    root /var/www/app;
-
-    location / {
-        index index.html;
-    }
-}
-EOF'
-
-sudo systemctl enable nginx
-sudo systemctl start nginx
-
-EOF
-}
-
 variable "common_tags" {
   type = map
 
